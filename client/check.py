5d79221404c318685ac54e532dd9ae40536f357a
morebeautifulthandoriangray <kate.ezhova2016@gmail.com>
2023-05-27 10:52:43 +0400

    final attempt

--- client/check.py
+++ client/check.py
@@ -1,30 +1,41 @@
-import http
+from flask import Flask, jsonify, request
 import requests
-
-from flask import Flask
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
-faulty_endpoint = "http://localhost:5000/fail"
-success_endpoint = "http://localhost:5000/data"
+failure_counter = 0
+failure_time = None
+time_for_recovery = timedelta(seconds=5)
+
+# faulty_endpoint = "http://localhost:5000/fail"
+# success_endpoint = "http://localhost:5000/data"
 random_status_endpoint = "http://localhost:5000/randfail"
 
 
+@app.route('/')
+def make_first_request():
+    return 'HEEEEEEYYYYY'
+
+
 @app.route('/status')
-def make_request(url):
+def make_request(url=random_status_endpoint):
+    global failure_counter, failure_time
+
+    if failure_time is not None and datetime.now() - failure_time < time_for_recovery:
+        return jsonify({'status': 'Not available'}), 503
+
     try:
         response = requests.get(url, timeout=0.3)
-        if response.status_code == http.HTTPStatus.OK:
-            print(f"Call to {url} succeed with status code = {response.status_code}")
-            return "response"
-        if 500 <= response.status_code < 600:
-            print(f"Call to {url} failed with status code = {response.status_code}")
-            raise Exception("Server Issue")
-    except Exception:
-        print(f"Call to {url} failed")
-        raise
+        response.raise_for_status()
+        return jsonify(response.json())
+    except requests.exceptions.RequestException:
+        failure_counter += 1
+        if failure_counter >= 5:
+            failure_time = datetime.now()
+            return jsonify({'status': 'Not available'}), 503
+
 
 
 if __name__ == '__main__':
     app.run()
-
--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 import random
 import time
 
+
 app = Flask(__name__)
 
 data = ["apple", "pineapple"]
@@ -9,22 +10,16 @@ data = ["apple", "pineapple"]
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'HEEEEEYYEYEYEYEY'
 
-
-@app.route('/data')
-def data_data():
-    return data, 200
-
-
-@app.route('/fail')
+@app.route('/fault')
 def faulty_endpoint():
     r = random.randint(0, 1)
     if r == 0:
         time.sleep(5)
 
     return {
-        "msg": "I will fail."
+        "msg": "I've failed."
     }, 500
 
 
@@ -32,9 +27,7 @@ def faulty_endpoint():
 def fail_randomly_endpoint():
     r = random.randint(0, 1)
     if r == 0:
-        return {
-            "msg": "Success msg"
-        }, 200
+        return data, 200
 
     return {
         "msg": "I will fail (sometimes)."
@@ -42,5 +35,4 @@ def fail_randomly_endpoint():
 
 
 if __name__ == '__main__':
-
-    app.run(port=5000)
+    app.run()
